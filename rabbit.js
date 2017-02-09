d1bbc6dea3b10498694ef56f4c66a582b6b8cd8b
Ankit Yadav <ankitwrk@gmail.com>
2017-02-09 15:00:20 +0530

    Modifying rabbit.js

--- rabbit.js
+++ rabbit.js
@@ -1,22 +1,12 @@
 var amqp = require('amqplib/callback_api');
 var exports = module.exports = {};
 
-// var rabbitchannel;
-// var exchange;
 amqp.connect('amqp://localhost',function (err, conn) {
-    
-    exports.connection=conn;
-    
     if (err){
-        console.log('Error connecting server',err);
-    }else{
-        console.log('Successfully connected creating channel');
-        conn.createChannel(function (err, ch) {
-            
-            var exchange='yoloexchange';
-            ch.assertExchange(exchange,'direct',{durable: true});
-            exports.exchange=exchange;
-            // rabbitchannel=ch;
-        });
+        console.error('Error connecting server ',err);
+    }
+    else{
+        console.warn('Successfully connected creating channel');
+        exports.connection=conn;
     }
 });
